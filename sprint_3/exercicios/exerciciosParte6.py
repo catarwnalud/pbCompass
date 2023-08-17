0c67189d6d7127208c0b103c5c316a63017c0595
unknown <catarinaludmilanf@gmail.com>
2023-08-17 16:32:36 -0400

    Exercícios

--- sprint_3/exercicios/exerciciosParte6.py
+++ sprint_3/exercicios/exerciciosParte6.py
@@ -122,3 +122,39 @@ class Calculo:
 Calculo.soma(4, 5)
 Calculo.sub(4, 5)
 
+#--------------------------------------------------------------------------------------------------------------------------------------#
+
+"""
+E24 - Crie uma classe Ordenadora que contenha um atributo listaBaguncada e que contenha os métodos ordenacaoCrescente e ordenacaoDecrescente.
+      Instancie um objeto chamado crescente dessa classe Ordenadora que tenha como listaBaguncada a lista [3,4,2,1,5] e instancie um outro objeto,
+      decrescente dessa mesma classe com uma outra listaBaguncada sendo [9,7,6,8].
+
+      Para o primeiro objeto citado, use o método ordenacaoCrescente e para o segundo objeto, use o método ordenacaoDecrescente.
+
+      Imprima o resultado da ordenação crescente e da ordenação decresce
+
+      [1, 2, 3, 4, 5] 
+      [9, 8, 7, 6]
+"""
+
+class Ordenadora:
+
+    def __init__(self, lista):
+        self.listaBaguncada = lista
+
+
+    def ordenacaoCrescente(self):
+        listaC = sorted(self.listaBaguncada)
+        return listaC
+
+
+    def ordenacaoDecrescente(self):
+        listaD = sorted(self.listaBaguncada)
+        return listaD[::-1]
+
+
+crescente = Ordenadora([3,4,2,1,5])
+print(crescente.ordenacaoCrescente())
+
+decrescente = Ordenadora([9,7,6,8])
+print(decrescente.ordenacaoDecrescente())
