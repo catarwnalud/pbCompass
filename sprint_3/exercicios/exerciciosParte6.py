473abe33f0430667dfc51f820178089040d59875
unknown <catarinaludmilanf@gmail.com>
2023-08-17 17:23:16 -0400

    Exercícios

--- sprint_3/exercicios/exerciciosParte6.py
+++ sprint_3/exercicios/exerciciosParte6.py
@@ -158,3 +158,49 @@ print(crescente.ordenacaoCrescente())
 
 decrescente = Ordenadora([9,7,6,8])
 print(decrescente.ordenacaoDecrescente())
+
+#--------------------------------------------------------------------------------------------------------------------------------------#
+
+"""
+E25 - Crie uma classe Avião que possua os atributos modelo, velocidade_maxima, cor e capacidade.
+      Defina o atributo cor de sua classe , de maneira que todas as instâncias de sua classe avião sejam da cor “azul”.
+
+      Após isso, a partir de entradas abaixo, instancie e armazene em uma lista 3 objetos da classe Avião.
+
+      Ao final, itere pela lista imprimindo cada um dos objetos no seguinte formato:
+
+      “O avião de modelo “x” possui uma velocidade máxima de “y”, capacidade para “z” passageiros e é da cor “w”.
+
+      Sendo x, y, z e w cada um dos atributos da classe “Avião”.
+
+      Valores de entrada:
+
+      modelo BOIENG456: velocidade máxima 1500 km/h: capacidade para 400 passageiros: Cor Azul
+
+      modelo Embraer Praetor 600: velocidade máxima 863km/h: capacidade para 14 passageiros: Cor Azul
+
+      modelo Antonov An-2: velocidade máxima de 258 Km/h: capacidade para 12 passageiros: Cor Azul
+"""
+
+class Aviao:
+
+
+    def __init__(self, modelo, velocidade, cap, cor = 'Azul'):
+        self.modelo = modelo
+        self.velocidade_maxima = velocidade
+        self.cor = cor
+        self.capacidade = cap
+
+
+    def info(self):
+        print("O avião de modelo {} possui uma velocidade máxima de {}, capacidade para {} passageiros e é da cor {}".format(self.modelo, self.velocidade_maxima, self.capacidade, self.cor))
+
+
+a = Aviao("BOIENG456", "1500 km/h", 400)
+b = Aviao("Embraer Praetor 600", "863km/h", 14)
+c = Aviao ("Antonov An-2", "258 Km/h", 12)
+
+lista = [a,b,c]
+
+for i in lista:
+      i.info()
