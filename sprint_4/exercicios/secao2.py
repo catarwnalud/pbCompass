55b0286faa7196a5bd1634339b68ac5d97a49a7b
unknown <catarinaludmilanf@gmail.com>
2023-08-26 17:25:06 -0400

    Exercícios

--- sprint_4/exercicios/secao2.py
+++ sprint_4/exercicios/secao2.py
@@ -67,3 +67,61 @@ def conta_vogais(texto:str)-> int:
 
 #--------------------------------------------------------------------------------------------------------------------------------------#
 
+"""
+E4- A função calcular_valor_maximo deve receber dois parâmetros, chamados de operadores e operandos.
+    Em operadores, espera-se uma lista de caracteres que representam as operações matemáticas suportadas (+, -, /, *, %),
+    as quais devem ser aplicadas à lista de operadores nas respectivas posições. Após aplicar cada operação ao respectivo par de operandos,
+    a função deverá retornar o maior valor dentre eles.
+
+    Veja o exemplo:
+
+    Entrada
+
+    operadores = ['+','-','*','/','+']
+    operandos  = [(3,6), (-7, 4.9), (8,-8), (10,2), (8,4)]
+
+    Aplicar as operações aos pares de operandos
+
+    [ 3+6 , -7-4.9, 8*-8 , 10/2 , 8+4 ] 
+
+    Obter o maior dos valores
+
+    12
+
+    Na resolução da atividade você deverá aplicar as seguintes funções:
+
+    max
+
+    zip
+
+    map
+"""
+
+
+def op(tupla):
+
+    if tupla[1] == '+':
+        return tupla[0][0] + tupla[0][1]
+    
+    elif tupla[1] == '-':
+        return tupla[0][0] - tupla[0][1]
+    
+    elif tupla[1] == '*':
+        return tupla[0][0] * tupla[0][1]
+    
+    else:
+        return tupla[0][0] / tupla[0][1]
+    
+    
+def calcular_valor_maximo(operadores, operandos) -> float:
+
+    resultado = list(map(op, zip(operandos, operadores)))
+
+    return max(resultado)
+
+operadores = ['+','-','*','/','+']
+operandos  = [(3, 6), (-7, 4.9), (8, -8), (10, 2), (8, 4)]
+
+print(calcular_valor_maximo(operadores, operandos))
+
+#--------------------------------------------------------------------------------------------------------------------------------------#
