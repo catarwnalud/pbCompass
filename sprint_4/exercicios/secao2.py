def75ac0cae74dbceca85d59ee96e65511c800b6
unknown <catarinaludmilanf@gmail.com>
2023-09-01 09:27:15 -0400

    Exercícios

--- sprint_4/exercicios/secao2.py
+++ sprint_4/exercicios/secao2.py
@@ -68,6 +68,46 @@ def conta_vogais(texto:str)-> int:
 #--------------------------------------------------------------------------------------------------------------------------------------#
 
 """
+E3- A função calcula_saldo recebe uma lista de tuplas, correspondendo a um conjunto de lançamentos bancários.
+    Cada lançamento é composto pelo seu valor (sempre positivo) e pelo seu tipo (C - crédito ou D - débito). 
+
+    Abaixo apresentando uma possível entrada para a função.
+
+    lancamentos = [
+        (200,'D'),
+        (300,'C'),
+        (100,'C')
+    ]
+
+    A partir dos lançamentos, a função deve calcular o valor final, somando créditos e subtraindo débitos. Na lista anterior, por exemplo, teríamos como resultado final 200.
+
+    Além de utilizar lambdas, você deverá aplicar, obrigatoriamente, as seguintes funções na resolução:
+
+    reduce (módulo functools)
+
+    map
+"""
+from functools import reduce
+
+
+def calcula_saldo(lancamentos) -> float:
+     
+
+    def rdc(tupla):
+        total = reduce(lambda total, v: total - v[0] if v[1] == 'D' else total + v[0], lancamentos, 0)
+        return total
+    
+    total = list(map(rdc, lancamentos))
+
+    return total[0]
+
+lancamentos = [(10, 'D'), (300, 'C'), (20, 'C'), (80, 'D'), (300, 'D')]
+
+print(calcula_saldo(lancamentos)) 
+
+#--------------------------------------------------------------------------------------------------------------------------------------#
+
+"""
 E4- A função calcular_valor_maximo deve receber dois parâmetros, chamados de operadores e operandos.
     Em operadores, espera-se uma lista de caracteres que representam as operações matemáticas suportadas (+, -, /, *, %),
     as quais devem ser aplicadas à lista de operadores nas respectivas posições. Após aplicar cada operação ao respectivo par de operandos,
