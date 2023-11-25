7b5683415a921489a9f62ddc3fca2aff2e2eb847
unknown <catarinaludmilanf@gmail.com>
2023-11-25 18:01:01 -0400

    Desafio

---
+++ sprint_7/exercicios/raw_zone.py
@@ -0,0 +1,33 @@
+import boto3
+import csv
+import datetime
+
+
+# Configuração do acesso ao bucket
+aws_access_key_id = ' '
+aws_secret_access_key = ' '
+bucket_name = ''
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+ 
+# Especifição dos arquivos
+local_movies_file = 'movies.csv'
+local_series_file = 'series.csv'
+
+# Camadas da Raw
+raw_layer = 'Raw'
+local_data = 'Local'
+data_format = 'CSV'
+data_specifications = ['Movies', 'Series']
+
+# Data
+today = datetime.date.today()
+year = today.year
+month = today.month
+day = today.day
+ 
+def upload_to_s3(local_file, data_specification):
+    s3_key = f'{raw_layer}/{local_data}/{data_format}/{data_specification}/{year}/{month}/{day}/{data_specification.lower()}.csv'
+    s3.upload_file(local_file, bucket_name, s3_key)
+ 
+upload_to_s3(local_movies_file, data_specifications[0])
+upload_to_s3(local_series_file, data_specifications[1])
